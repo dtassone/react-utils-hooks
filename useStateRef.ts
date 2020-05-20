438e5c6068fb821257e8ef37f9e881d52954f600
damien tassone <damien.tassone@gmail.com>
2020-05-20 18:13:55 +0200

    Update useStateRef.ts

--- useStateRef.ts
+++ useStateRef.ts
@@ -1,10 +1,12 @@
-/*
+import { RefObject, useRef, useState } from 'react';
+
+/**
  * allow to put a forwarded ref in the dependency array of an effect and do something when current change
  */
-function useStateRef<T>(forwaredRef: RefObject<T>) {
+export function useStateRef<T>(forwaredRef: RefObject<T>) {
   const [refState, setRefState] = useState(forwaredRef.current);
   const prevColumnsRef = useRef<T | null>(null);
-  if(prevColumnsRef && forwaredRef && prevColumnsRef.current !== forwaredRef.current) {
+  if (prevColumnsRef && forwaredRef && prevColumnsRef.current !== forwaredRef.current) {
     prevColumnsRef.current = forwaredRef.current;
     setRefState(forwaredRef.current);
   }
